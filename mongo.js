a67b901e42b6507f9a283218e5dcdec38f4b5a49
dipenkukadiya <dipenkukadiya@gmail.com>
2024-01-10 23:54:07 +0530

    crud node

--- mongo.js
+++ mongo.js
@@ -2,17 +2,40 @@ const MongoClient =require('mongodb').MongoClient;
 const url ='mongodb://127.0.0.1:27017';
 const database ='college';
 const client = new MongoClient(url);
+const { name } = require('ejs');
 const express = require('express');
 const app= express();
 
+const insert =async ()=>{
+let result =await client.connect();
+let db =  result.db(database);
+//  let collection =db.collection('student');
+const resultt=await db.collection('student').insertMany([{name:'dipen',age:'12',mail:'asd'}],function(err,res){console.log("inserted");});
+console.log(resultt);
+}
+insert();
 async function getData(){
 let result =await client.connect();
 let db =  result.db(database);
 let collection =db.collection('student');
-let response = await collection.find({age:'14'}).toArray();
+let response = await collection.find({}).toArray();
 console.log(response);
 }
 getData();
+   
+const update =async()=>{
+let result =await client.connect();
+let db =  result.db(database);
+let updated = await db.collection('student').updateOne({age:'10'},{$set:{name:'helloooo'}})
+}
+update()
+const deletee = async ()=>{
+let result =await client.connect();
+let db =  result.db(database);
+let a = await db.collection('student').deleteMany({name:'dipen'})
+}
+deletee();
+
 // const { MongoClient } = require('mongodb');
 // const url = 'mongodb://localhost:27017';
 
